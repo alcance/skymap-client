16d2516a1dca3c1528edef4d2eb75ff2de7f7ecc
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-30 05:40:37 -0800

    Locations resource get and post record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 import * as io from 'socket.io-client';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import * as chance from 'chance';
 
 @Component({
   selector: 'app-root',
+  providers: [AppService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   private socket;
+  private chance;
+  markers: marker[] = [];
 
-  constructor(private _http: Http) {
+  constructor(private _appService: AppService) {
     this.socket = io.connect('0.0.0.0:8001');
+    this.chance = chance();
+  }
+  
+  // Subscribe to Service
+  ngOnInit() {
+    this._appService.getLocations()
+      .subscribe(responseAppData => this.markers = responseAppData);
+  }
+
+  getRandomLoc(from, to, fixed) {
+    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
   }
 
   sendLocation() {
-    this.socket.emit('locations', {"lat": 1234.22, "lng": 123123.23});
+    this.socket.emit('locations', {"lat": this.chance.latitude(), "lng": this.chance.altitude(), "label": "w00t", "draggable": false});
   }
   
   title = 'Skymap';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-  markers: marker[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: false
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
+  lat: number = 31.015279;
+  lng: number = -80.523438;
+  zoom: 9;
 }
 interface marker {
   lat: number;
