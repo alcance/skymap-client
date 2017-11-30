912f087c005217ace7fc7d576f3f5dcd955817b3
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-30 01:38:10 -0800

    Create socket connection to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.socket = io.connect('0.0.0.0:8001');
+  }
+  
   title = 'Skymap';
   lat: number = 51.678418;
   lng: number = 7.809007;
